493110b44be177ada59fc4eb426b0aa26df21211
Bryan <bryan@denproductions.com>
2020-07-14 13:26:13 -0400

    updated links

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<link rel="shortcut icon" href="images/statue64.ico" />
+<title>Welcome to Den Productions</title>
+<head>
+    <title>Layout Master</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="js/denpro.js"></script>
+</head>
+<body>
+    <div class="mastHead sticky">
+        <div id="siteName">Den Productions</div>
+        <nav class="">
+            <ul class="globalNav">
+                <li class="globalLink"><a href="../index.php">Home</a></li>
+                <li class="globalLink"><a href="../about.php">About</a></li>
+                <li class="globalLink"><a href="../services.php">Services</a></li>
+                <li class="globalLink"><a href="index.php">Samples</a></li>
+                <li class="globalLink"><a href="../links.php">Links</a></li>
+                <li class="push globalLink"><a href="mailto:Bryan@DenProductions.com" target="_blank">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="welcomeHeader"><script language="javascript">document.write(parent.getTodaysDate());</script></div>
+    </div>
+
+    <div class="mainPage">
+        <div class="zone blue grid-wrapper">
+            <div class="box2 zone">
+                <p>Custom service for the individual or small business.<br>Serving the needs of the individual and small business with the respect of big business!</p>
+                <p>Located in Chesapeake, Virginia<br>Connecting you to the world!</p>
+            </div>
+            <div class="box3 zone">
+              <h3>Den Productions Samples!</h3>
+              <p>
+              <center>
+              <br><a href="http://greatbridgehigh.com" id="bodyLink"><img src="images/gbh.png"><br>Great Bridge High</a><br>
+              <br><a href="https://github.com/bdrusnak" id="bodyLink"><img src="images/github.png"><br>GitHub</a><br><br>
+            </center>
+              </p>
+            </div>
+        </div>
+        <footer class="zone blue">
+            <script language='javascript'>document.write(parent.copyright());</script>
+        </footer>
+    </div>
+</body>
+</html>
